74ebc23da2e129501ca51ebd00e32b87d8466c8d
Rajnish Kumar <110758110+kr-rajnish@users.noreply.github.com>
2024-08-14 11:49:45 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -58,7 +58,7 @@ export default function Home() {
                   className="home__blob-img"
                   x="0"
                   y="0"
-                  href="assets/img/myImageR.jpg"
+                  href="assets/img/MyImage3.jpg"
                 />
               </g>
             </svg>
